a88fd561fcdbab8666df0f5c3cc4032f646e7b4f
wmc2tokyo <wmcooper2@yahoo.com>
2020-05-31 14:42:45 +0900

    Got MongoDB setup. Added an HTML form Will do DB queries (GET/POST) next.

--- app.js
+++ app.js
@@ -1,41 +1,74 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require("http-errors");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const indexRouter = require("./routes/index");
+const usersRouter = require("./routes/users");
+const app = express();
+const bodyParser = require("body-parser");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const MongoClient = require("mongodb").MongoClient;
+const assert = require("assert");
 
-var app = express();
+// Connection URL
+const url = "mongodb://localhost:27017";
+// Database Name
+const dbName = "myproject";
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// Use connect method to connect to the server
+MongoClient.connect(url, { useUnifiedTopology: true }, function (err, client) {
+  assert.equal(null, err);
+  console.log("Connected successfully to server");
+  const db = client.db(dbName);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+
+  ///to pass the db around
+  console.log("Connected to Database.");
+  const opinions = db.collection("simplesurveyv2");
+  app.use((req, res, next) => {
+    res.locals.opinions = opinions;
+    next();
+  });
+
+
+
+
+
+
+
+  app.use("/", indexRouter);
+  app.use("/users", usersRouter);
+
+  // catch 404 and forward to error handler
+  app.use(function (req, res, next) {
+    next(createError(404));
+  });
+
+  // error handler
+  app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get("env") === "development" ? err : {};
+
+    // render the error page
+    res.status(err.status || 500);
+    res.render("error");
+  });
+
+  client.close();
 });
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", (req, res, next) => {
+  res.render("index", { documents: 3 });
+});
+
+router.post("/submitform", (req, res, next) => {
+  //put data into database
+  // res.json({ "your data": req.body });
+  res.redirect("/thankyou");
+});
+
+router.get("/test/:banana", (req, res, next) => {
+  res.json({ "you posted": req.params });
+});
+
+router.get("/thankyou", (req, res, next) => {
+  res.render("thankyou");
 });
 
 module.exports = router;
